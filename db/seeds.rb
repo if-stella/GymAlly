85a2a04f7aab703eee84a60906ea182454d95bf3
Stella Raab <hallo@stellaraab.de>
2022-03-01 13:01:03 +0100

    Finished user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+
+puts 'Creating 25 fake users...'
+25.times do
+  user = User.new(
+    username: Faker::Internet.username(5..8),
+    age: rand(18..39),
+    email: Faker::Internet.safe_email,
+    gender: Faker::Gender.type,
+    quote: Faker::Hipster.sentence
+  )
+  user.save!
+end
+puts 'Finished with the users!'
