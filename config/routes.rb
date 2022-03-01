b7683ccab3a721e1809ebd069b4b862f19a81bde
Kang Hsieh <kanghsieh1990@gmail.com>
2022-03-01 11:33:04 +0100

    redone routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "/dashboard", to: "pages#dashboard", as: :dashboard
-  resources :users, only: %i[index show edit update] do
-    resources :friendships, only: %i[create update]
-    resources :chatrooms, only: %i[index show]
-  end
+  resources :users, only: %i[index show edit update]
+  resources :friendships, only: %i[create update]
+  resources :chatrooms, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
