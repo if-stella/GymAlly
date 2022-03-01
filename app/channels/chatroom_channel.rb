d293459bac7091d0da53997507874592dd8be953
Kang Hsieh <kanghsieh1990@gmail.com>
2022-03-01 22:36:49 +0100

    added active cable functionality to chatrooms

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,11 @@
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+    chatroom = Chatroom.find(params[:id])
+    # stream_from "some_channel"
+    stream_for chatroom
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+import consumer from "../channels/consumer"
+
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages", "form"]
+
+  connect() {
+    console.log("Subscribed from the chatroom")
+    this.channel = consumer.subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
+    // console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    this.formTarget.reset()
+  }
+}
