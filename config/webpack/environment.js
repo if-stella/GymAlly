006acffb01fd922da5f61949108d6a5be2dac64f
Kang Hsieh <kanghsieh1990@gmail.com>
2022-03-02 18:17:15 +0100

    fixed bug with santi...

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// Preventing Babel from transpiling NodeModules packages
-environment.loaders.delete('nodeModules');
 module.exports = environment
