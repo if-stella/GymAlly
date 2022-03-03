3898aec76a72694ee4cdb54bc302103b7279f1b2
Ida Schwarzkopf <ida.schwarzkopf@ha-es.de>
2022-03-03 04:46:59 +0100

    user can only view their own chats

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,10 @@
 class ChatroomsController < ApplicationController
-
   def index
     # only show chatrooms of current user
-    @chatrooms = Chatroom.all
+    @chatrooms = []
+    Chatroom.all.each do |c|
+      c.friendship.user_one == current_user || c.friendship.user_two == current_user ? @chatrooms << c : @chatrooms
+    end
     @invitations = Friendship.where(user_two: current_user).where(status: "pending")
     @requests = Friendship.where(user_one: current_user).where(status: "pending")
     @friendships = Friendship.where("user_one_id = #{current_user.id} or user_two_id = #{current_user.id}").where(status: "accepted")
@@ -11,5 +13,10 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  def create
+    @chatroom = Chatroom.create(friendship_id: params[:id])
+    redirect_to chatroom_path(@chatroom)
   end
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,6 @@
 class Chatroom < ApplicationRecord
   belongs_to :friendship
   has_many :messages, dependent: :destroy
+  has_one :user_one, through: :friendship
+  has_one :user_two, through: :friendship
 end
