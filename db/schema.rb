95e614c3432623ff8a3966866e14fa3ac4b11a65
Kang Hsieh <kanghsieh1990@gmail.com>
2022-03-07 15:08:10 +0100

    added links to navbar icons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_03_04_200319) do
 
   # These are extensions that must be enabled in order to support this database
