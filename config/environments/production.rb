5dee0f6e6922c59e86dc1077b441c8264f916ec9
Stella Raab <hallo@stellaraab.de>
2022-09-03 19:47:06 +0200

    Added assets to assets folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,17 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+    # Disable Rails's static asset server (Apache or nginx will already do this)
+config.serve_static_assets = false
+
+# Compress JavaScripts and CSS
+config.assets.compress = true
+
+# Don't fallback to assets pipeline if a precompiled asset is missed
+config.assets.compile = false
+
+# Generate digests for assets URLs
+config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
